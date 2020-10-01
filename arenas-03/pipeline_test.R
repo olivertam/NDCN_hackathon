0afc3521f50d5b869d8b636d7563b5f0f9f117d6
jules32 <lowndes@nceas.ucsb.edu>
2020-10-01 10:00:57 -0700

    This looks great! Some documenting and here() to help arenas lab reduce filepath woes

--- arenas-03/pipeline_test.R
+++ arenas-03/pipeline_test.R
@@ -1,28 +1,35 @@
-## name_checker_usage.R
-## Testing using name_checker.R
+## pipeline_test.R
+## Establishing a pipeline for file checking and moving for the Arenas Lab
 
 
 ## setup ----
+library(here) # install.packages("here")
+code_path <- "arenas-03/arenasImageSorter/R" 
+log_path <- "arenas-03/logs"
 
-source("arenasImageSorter/R/name_checker.R")
-source("arenasImageSorter/R/file_mover.R")
-source("arenasImageSorter/R/file_anonymizer.R")
+source(here(code_path, "name_checker.R"))
+source(here(code_path, "file_mover.R"))
+source(here(code_path, "file_anonymizer.R"))
 
 ### Test run ###
 
-dir.create("logs")
+## Create a "logs" folder if it doesn't already exist
+if (!dir.exists(here(log_path))) dir.create(here(log_path))
 
+## run name_checker() and save log file
 log = name_checker("files",print2screen=FALSE)
-write.table(log,"logs/name_checking_log.txt",sep="\n",quote=F,row.names=F,col.names=F)
+write.table(log, here(log_path, "name_checking_log.txt"), 
+            sep="\n",quote=F,row.names=F,col.names=F)
 
+## run file_mover() and save log file
 log = file_mover("files","moved")
 warnings()
-write.table(log,"logs/file_moving_log.txt",sep="\n",quote=F,row.names=F,col.names=F)
+write.table(log, here(log_path, "file_moving_log.txt"), 
+            sep="\n",quote=F,row.names=F,col.names=F)
 
+## run filemover(), anonymize files, and save log file
 log = file_mover("files","anonymized",anonymize=TRUE)
 warnings()
-write.table(log,"logs/file_moving_anonymized_log.txt",sep="\n",quote=F,row.names=F,col.names=F)
-
-
-
+write.table(log,here(log_path, "file_moving_anonymized_log.txt"),
+            sep="\n",quote=F,row.names=F,col.names=F)
 
